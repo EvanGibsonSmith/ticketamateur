ebb60d8846757967a80eead78e326685a9df7330
EvanGibsonSmith <114107230+EvanGibsonSmith@users.noreply.github.com>
2023-11-27 16:05:22 -0500

    Fixed venue GUI for createVenue

--- src/AuthenticatePage.js
+++ src/AuthenticatePage.js
@@ -9,7 +9,7 @@ export function Authenticate() {
             <h1>This is the authenticate page, while we will need to actually authenticate later, below are buttons to send to other pages</h1>
             <div className="flex-container-row">
                 <p>Authenticate:    </p>
-                <input type="text" id="name" name="name" height="2" required minlength="1" maxlength="10" size="10" />
+                <input type="text" id="authenticatePageID" name="name" height="2" required minlength="1" maxlength="20" size="10" />
             </div>
 
             <div>
