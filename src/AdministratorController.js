ae087a3d20a13480dc1412e62158e57557198aa5
TimoryG <127773359+TimoryG@users.noreply.github.com>
2023-11-28 10:26:35 -0500

    Update AdministratorController.js

--- src/AdministratorController.js
+++ src/AdministratorController.js
@@ -1,3 +1,25 @@
+import { get } from "./Api"
+
+export function listVenues() { 
+    get('/constants')
+        .then(function (response) {
+
+            let str = ''
+            for (let c of response.constants) {
+                str += c.name + '=' + c.value + '<br>'
+            }
+
+            // insert HTML in the <div> with 
+            // constant-list
+            let cd = document.getElementById('constant-list')
+            cd.innerHTML = str
+
+        })
+        .catch(function (error) {
+            // not much to do
+            console.log(error)
+        })
+}
 
 export function deleteShow() {
     console.log("Test delete successful")
