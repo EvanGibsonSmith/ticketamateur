59a3bfc73f9e23ebe9df135e520208b93cc165de
EvanGibsonSmith <114107230+EvanGibsonSmith@users.noreply.github.com>
2023-11-27 23:57:01 -0500

    Added URL to fetch, put in Axios to try

--- src/AuthenticateController.js
+++ src/AuthenticateController.js
@@ -1,10 +1,25 @@
 import { useNavigate } from "react-router-dom";
 
+import { axios } from "axios"; // TODO remove if not used eventually, might be easier
+
+const URL = "REALURL";
+
+// TODO later make this imported in multiple places so that URL doesn't need to be copied in a bunch of places if using axios. Use import port from API like he has in many of his examples
+const instance = axios.create({ 
+  baseURL: URL // TODO make actual URL
+});
 
 export function authenticateUser (authPageToken) {
     console.log(authPageToken)
     let payload = {"authToken": authPageToken}
-    const response = fetch(
+
+    // TODO I have this axios way in here just to see how they compare for now. This way might be easier, following his hangman example
+    //instance.post('/authenticate').then((response) => {
+    //  model.setInfo(response.data.gameID, response.data.wordLength)
+    //  forceRedraw(redraw+1)
+    //})
+
+    const response = fetch(URL, 
         {method: "POST", 
         body: JSON.stringify(payload)}).then((response) => response)
 
@@ -14,9 +29,12 @@ export function authenticateUser (authPageToken) {
             let result = await val.json()
             switch(result.userType) {
                 case "venueManager":
+                  console.log("VENUEMANAGERTYPE")
                   //navigate("/venuemanager"); TODO make this work
                   break;
                 case "administrator":
+                  console.log("ADMINTYPE")
+
                   //navigate("/admin"); TODO make this work
                   break;
                 default:
