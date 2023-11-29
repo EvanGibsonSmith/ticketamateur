0c132c1cd66a736c68116bef69d1eecbbef2a206
TimoryG <127773359+TimoryG@users.noreply.github.com>
2023-11-28 20:20:25 -0500

    Return to authenticate page button

--- src/ConsumerPage.js
+++ src/ConsumerPage.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { useNavigate } from "react-router";
 
 export function Consumer() {
+    const navigate = useNavigate();
     return (
         <body>
             <h1>This is the consumer page</h1>
+            <button onClick={e => navigate("/")}>Return to Authenticate Page</button>
         </body>
     )
 }
