cda9d01f9f44d51e40859fd5510c4a86926e6cef
EvanGibsonSmith <114107230+EvanGibsonSmith@users.noreply.github.com>
2023-12-06 13:01:18 -0500

    Small things?

--- src/Controller/AdministratorController.js
+++ src/Controller/AdministratorController.js
@@ -24,7 +24,7 @@ export function showReport(authToken) {
 
             let str = ''
             for (let c of response.constants) {
-                str += "Name Of Venue: "+ c.venueName + ", Name Of Show: " + c.showName + ", Show ID: " + c.showID + '<br>'
+                str += "Name Of Venue: "+ c.venueName + ", Name Of Show: " + c.showName + '<br>'
             }
 
             // insert HTML in the <div> with 
--- src/Pages/ConsumerPage.js
+++ src/Pages/ConsumerPage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router";
 import "./ConsumerPage.css";
-import { totalPrice, searchActiveShows, availableSeats, purchaseSeats, listActiveShows } from "../Controller/CustomerController";
+import { totalPrice, searchShow, availableSeats, purchaseSeats, listShows } from "../Controller/CustomerController";
 
 export function Consumer() {
     const navigate = useNavigate();
@@ -9,38 +9,34 @@ export function Consumer() {
         <body>
             <h1>This is the consumer page</h1>
             <div className="flex row align-flex-start">
+                <div className="flex align-center column width50 height50 align-flex-start">
+                    <div id="venueDisplay" className="flex column-reverse margin-vertical venueHolder" style={{width:"400px", height: "400px"}}>
+                        <p>Seat Test 1</p>
+                        <p>Seat Test 2</p>
+                        <p>Seat Test 3</p>
+                        <p>Seat Test 4</p>
+                    </div>
+                    <button id='searchshow' onClick={e => searchShow()}>Search Show</button>
+                </div>
                 <div className="flex column justify-spaced align-center margin">
                     <text id="total-price">Total Price: {totalPrice}</text>
                     <select className="flex column align-left scroll margin-children" style={{fontSize :"25px",width:"300px", backgroundColor: "gainsboro"}}>
-                        <option className="show-display">Test Show 1</option>
-                        <option className="show-display">Test Show 2</option>
-                        <option className="show-display">Test Show 3</option>
-                        <option className="show-display">Test Show 4</option>
+                    <option className="show-display">Test Show 1</option>
+                    <option className="show-display">Test Show 2</option>
+                    <option className="show-display">Test Show 3</option>
+                    <option className="show-display">Test Show 4</option>
                     </select>
                     <button className="right-side-buttons" onClick={e => availableSeats()}>Available Seats</button>
                     <button className="right-side-buttons" onClick={e => purchaseSeats()}>Purchase Seats</button>
-                    <button className="right-side-buttons" onClick={e => listActiveShows()}>List Shows</button>
+                    <button className="right-side-buttons" onClick={e => listShows()}>List Shows</button>
                 </div>
-                <div className="flex column pad align-center">
-                    <input id="searchshowinput" type="text" style={{width: "400px"}} placeholder="Search Show"></input>
-                    <button id='searchshow' style={{width: "150px"}} onClick={e => searchActiveShows()}>Search Show</button>
-                    <select id="customerShowsList" className="flex column align-left scroll margin-children" style={{width: "500px", height: "600px", backgroundColor: "gainsboro", display: "block"}}>
-                        <option className="show-display">Test Show 1</option>
-                        <option className="show-display">Test Show 2</option>
-                        <option className="show-display">Test Show 3</option>
-                        <option className="show-display">Test Show 4</option>
-                    </select>
+                <div className="flex column align-left scroll margin-children" style={{width: "500px", height: "600px", backgroundColor: "gainsboro"}}>
+                    <text className="show-display">Test Show 1</text>
+                    <text className="show-display">Test Show 2</text>
+                    <text className="show-display">Test Show 3</text>
+                    <text className="show-display">Test Show 4</text>
                 </div>
-                <div id="seatsDisplay" className="flex column pad margin" style={{width:"200px", paddingLeft:"10px"}}>
-                    <h2 type="text" style={{width: "400px"}} placeholder="Search Seats"> Seats </h2>
-                    <button id='showseats' style={{width: "150px"}} onClick={e => availableSeats()}>Show Available Seats</button>
-                    <select multiple id="seatsList" className="flex column align-left scroll margin-children" style={{width: "500px", height: "600px", backgroundColor: "gainsboro"}}>
-                        <option className="seat">A1</option>
-                        <option className="seat">A2</option>
-                        <option className="seat">A3</option>
-                        <option className="seat">A4</option>
-                    </select>
-                </div>
+
             </div>
             <button onClick={e => navigate("/")}>Return to Authenticate Page</button>
         </body>
