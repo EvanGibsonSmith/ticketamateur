ab319127f5386e811caa79f73804abdbb435243b
EvanGibsonSmith <114107230+EvanGibsonSmith@users.noreply.github.com>
2023-12-06 13:10:16 -0500

    Revert "Small things?"
    This reverts commit cda9d01f9f44d51e40859fd5510c4a86926e6cef.

---
+++ xLambdaIndexes/listactiveshows/index.js
@@ -0,0 +1,34 @@
+const mysql = require('mysql');
+const db_access = require('/opt/nodejs/db_access')
+
+exports.handler = async (event) => {
+  
+  // get credentials from the db_access layer (loaded separately via AWS console)
+  var pool = mysql.createPool({
+      host: db_access.config.host,
+      user: db_access.config.user,
+      password: db_access.config.password,
+      database: db_access.config.database
+  });
+  let listActiveShows = () => {
+      return new Promise((resolve, reject) => {
+          pool.query("SELECT * FROM Shows where activated = ?", [1], (error, rows) => {
+            if (error) { return reject(error); }
+            return resolve(rows);
+        })
+      })
+  }
+  let response = undefined
+    const all_shows = await listActiveShows()
+    response = {
+        statusCode: 200,
+        constants: all_shows,
+    } 
+  
+  
+    
+  
+  pool.end()     // close DB connections
+
+  return response;
+}
