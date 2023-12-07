0af2ccaea9c26bf9b7c59de143faef8badca4df9
TimoryG <127773359+TimoryG@users.noreply.github.com>
2023-12-07 17:34:38 -0500

    Revenue equation for show report

--- src/Controller/AdministratorController.js
+++ src/Controller/AdministratorController.js
@@ -24,7 +24,7 @@ export function showReport(authToken) {
 
             let str = ''
             for (let c of response.constants) {
-                str += "Name Of Venue: "+ c.venueName + ", Name Of Show: " + c.showName + ", Show ID: " + c.showID + ", Price: " + c.showPrice + '<br>'
+                str += "Name Of Venue: "+ c.venueName + ", Name Of Show: " + c.showName + ", Show ID: " + c.showID + ", Revenue: $" + c.showPrice*c.seatsSold + '<br>'
             }
 
             // insert HTML in the <div> with 
