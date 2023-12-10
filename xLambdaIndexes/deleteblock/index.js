e2b4b1c427777ead29c570b829cfa3dd154503be
TimoryG <127773359+TimoryG@users.noreply.github.com>
2023-12-10 15:07:39 -0500

    Delete Block Backend

---
+++ xLambdaIndexes/deleteblock/index.js
@@ -0,0 +1,87 @@
+const mysql = require('mysql');
+const db_access = require('/opt/nodejs/db_access')
+
+exports.handler = async (event) => {
+  
+  // get credentials from the db_access layer (loaded separately via AWS console)
+  var pool = mysql.createPool({
+      host: db_access.config.host,
+      user: db_access.config.user,
+      password: db_access.config.password,
+      database: db_access.config.database
+  });
+  let isVenueManager = (auth) => {
+    return new Promise((resolve, reject) => {
+        pool.query("SELECT * FROM Venues WHERE authKey=?", [auth], (error, rows) => {
+            if (error) { return reject(error); }
+            console.log(rows)
+            if ((rows) && (rows.length == 1)) {
+                return resolve(true); 
+            } else {
+                return resolve(false);
+            }
+        });
+    });
+  }
+  let deleteBlock = (id) => {
+      return new Promise((resolve, reject) => {
+            pool.query("DELETE FROM Blocks WHERE blockID=?", [id], (error, rows) => {
+                if (error) { return reject(error); }
+                if ((rows) && (rows.affectedRows == 1)) {
+                    return resolve(true);
+                } else {
+                    return resolve(false);
+                }
+            });
+      });
+  }
+  
+  let isShowActive = (id) => {
+    return new Promise((resolve, reject) => {
+          pool.query("SELECT * FROM Shows WHERE showID=? AND activated=0", [id], (error, rows) => {
+              if (error) { return reject(error); }
+              if ((rows) && (rows.affectedRows == 1)) {
+                  return resolve(true);
+              } else {
+                  return resolve(false);
+              }
+          });
+    });
+}
+  let response = false
+  let authed = await isVenueManager(event.authToken)
+  let active = await isShowActive(event.showID)
+  if(authed){
+    if(active){
+        result = await deleteBlock(event.blockID)
+    }
+    if(result){
+        response = {
+        statusCode: 200,
+        
+        body: JSON.stringify(result)
+        }
+    }else{
+        response = {
+            statusCode: 400,
+            error: "No Block Under That ID that is Inactive"
+            }
+    }
+  }else{
+    response = {
+        statusCode: 400,
+        error: "Not Authorized"
+        }
+
+  }
+    
+
+   
+    
+  
+    pool.end()   // disconnect from database to avoid "too many connections" problem that can occur
+
+  return response;
+}
+
+
